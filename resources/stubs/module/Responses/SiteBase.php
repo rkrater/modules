4e88a3f8824fa321332d8d4cd7c570c9fc21b6ce
Rob Krater <rkrater@wcboe.org>
2017-11-03 14:27:46 -0400

    Update base

--- resources/stubs/module/Responses/SiteBase.php
+++ resources/stubs/module/Responses/SiteBase.php
@@ -9,10 +9,16 @@ abstract class SiteBase extends BaseAdminResponse
     public function __construct()
     {
         parent::__construct();
+
+        $this->getSite()->setTitle("DummyName");
+        $this->getBreadCrumbs()->item("DummyName");
+        $this->getMenuMain()->item('Index', route('DummySlug.index'), 'icon-home');
     }
 
     protected function notifications()
     {
-
+        $this->getNotifications()->group("DummyName", 'icon-home')->section("DummyName")->items([
+            new Item("icon-home", "DummyName", route('DummySlug.index'))
+        ]);
     }
 }
